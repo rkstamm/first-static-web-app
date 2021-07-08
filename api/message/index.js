172e132d31ffd499b3bd9ea279fd2178f3653ba0
Ron Stamm <rons@USA2021044.usa.newaysintl.com>
2021-07-08 17:10:22 -0600

    uuid stuff

---
+++ api/message/index.js
@@ -0,0 +1,21 @@
+module.exports = async function (context, req) {
+
+    // var uuid = new duuid.DeviceUUID().get();
+    // console.log(uuid);
+
+    context.res.json({
+        text: "Hello."
+    });
+    
+    // context.log('JavaScript HTTP trigger function processed a request.');
+
+    // const name = (req.query.name || (req.body && req.body.name));
+    // const responseMessage = name
+    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    // context.res = {
+    //     // status: 200, /* Defaults to 200 */
+    //     body: responseMessage
+    // };
+}
